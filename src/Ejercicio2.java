d6dc5ed3e1644c6b3c8aaafc52fbc67589a53e3a
Leandro Occhiato <leandro.occhiato@gmail.com>
2021-06-02 23:47:50 -0300

    [ADD] Se agrega completo ejercicio 2, y base de ejercicio 3

---
+++ src/Ejercicio2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    /*Ejercicio 2
+    Escribir una función que reciba tres números y devuelva el máximo entre los tres números.
+            Integer maximoEntreTresNumeros(Integer unNumeroA, Integer unNumeroB, Integer
+            unNumeroC)
+    Luego hacer un programa que permita el ingreso de 3 valores, utilice la función y muestre
+    el resultado.
+    */
+
+    public static void main(String[] args) {
+        Integer[] numeros = obtenerNumeros(3);
+
+        Integer maximo = maximoEntreTresNumeros(numeros[0], numeros[1], numeros[2]);
+
+        System.out.println("El maximo es: " + maximo);
+    }
+
+    private static Integer[] obtenerNumeros(int cantidad) {
+        Scanner scanner = new Scanner(System.in);
+        Integer[] numeros = new Integer[3];
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println("Ingrese numero " + (i+1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+        return numeros;
+    }
+
+    public static Integer maximoEntreTresNumeros(Integer unNumeroA, Integer unNumeroB, Integer
+            unNumeroC) {
+
+        return Math.max(Math.max(unNumeroA, unNumeroB), unNumeroC);
+    }
+}
